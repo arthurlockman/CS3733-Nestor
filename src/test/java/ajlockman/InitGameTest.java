946a1376dddb351b9bc17a387b751494034abd1c
Arthur Lockman <hello@rthr.me>
2015-04-09 20:44:58 -0400

    Added initialization tests.

---
+++ src/test/java/ajlockman/InitGameTest.java
@@ -0,0 +1,24 @@
+package ajlockman;
+
+import junit.framework.TestCase;
+import ks.client.gamefactory.GameWindow;
+import ks.common.model.Deck;
+import ks.launcher.Main;
+
+public class InitGameTest extends TestCase
+{
+    public void testGameInit()
+    {
+        Nestor nestor = new Nestor();
+        GameWindow gw = Main.generateWindow(nestor, Deck.OrderByRank);
+        gw.setVisible(true);
+
+        assertEquals("ajlockman-Nestor", nestor.getName());
+        assertEquals(0, nestor.getNumLeft().getValue());
+        assertEquals(0, nestor.getScore().getValue());
+        for (int i = 0; i < 8; i++)
+            assertEquals(6, nestor.tableau[i].count());
+        assertEquals(4, nestor.reserve.count());
+        assertEquals(false, nestor.hasWon());
+    }
+}
