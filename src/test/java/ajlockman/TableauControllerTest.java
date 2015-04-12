29763e4f463b717b26f98605c763e1d9279115ff
Arthur Lockman <hello@rthr.me>
2015-04-12 14:05:43 -0400

    Added shells for tests.

--- src/test/java/ajlockman/TableauControllerTest.java
+++ src/test/java/ajlockman/TableauControllerTest.java
@@ -1,5 +1,45 @@
 package ajlockman;
 
+import ks.client.gamefactory.GameWindow;
+import ks.common.model.Deck;
+import ks.launcher.Main;
+
 public class TableauControllerTest extends KSTestCase
 {
+    Nestor nestor;
+    GameWindow gw;
+
+    @Override
+    public void setUp()
+    {
+        nestor = new Nestor();
+        gw = Main.generateWindow(nestor, Deck.OrderByRank);
+        gw.setVisible(true);
+    }
+
+    @Override
+    public void tearDown()
+    {
+        gw.dispose();
+    }
+
+    public void testReserveToTableauMove()
+    {
+
+    }
+
+    public void testTableauToReserveMove()
+    {
+
+    }
+
+    public void testTableauToTableauMove()
+    {
+
+    }
+
+    public void testInvalidMove()
+    {
+
+    }
 }
